0e64e8b535e1eb80a8d20556a598ac2e303fb45f
chenweiyan <chanweiyan007@gmail.com>
2020-07-10 22:23:44 +0800

    使用 Provider 定义容器组件

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import CalculatorComp from './components/CalculatorComp';
+import { Provider } from 'react-redux';
 import reducerCalculator from './reducers/reducerCalculator';
+import { App } from './containers/App';
 
 const store = createStore(reducerCalculator);
 const rootEle = document.getElementById('root');
 
 const render = () => ReactDOM.render(
-  <CalculatorComp
-    s={store.getState()}
-    vstore={store}
-  />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   rootEle
 )
 
